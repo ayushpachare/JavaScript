e3f287445a9712b4d5bd9d0acad64543f55e0212
Ayush pachare <114282395+ayushpachare@users.noreply.github.com>
2024-10-19 00:46:43 +0530

    Day 1 of my 60-Day JavaScript Learning Streak
    Today, I dove into two important JavaScript concepts: let and const!
    
    🔹 let allows you to declare variables that can be reassigned later in the code. It’s block-scoped, which means it’s only accessible within the block where it’s defined—very useful for maintaining clean and error-free code. 💡
    
    🔹 const is used for variables that won’t change throughout your program. Once assigned, the value cannot be reassigned. Like let, it’s also block-scoped. Perfect for constants and values that stay the same!

---
+++ lec1.js
@@ -0,0 +1,11 @@
+const accountId = 12346
+let accountEmail = "ayush@gmail.com"
+var accountPassword = "12345"
+accountcity = "nagpur"
+let accountState
+
+accountEmail = "pachare@gmial.com"
+accountPassword = "453546"
+accountCity = "amravathi" 
+
+console.table([accountId , accountEmail , accountPassword ,accountCity , accountState ])
